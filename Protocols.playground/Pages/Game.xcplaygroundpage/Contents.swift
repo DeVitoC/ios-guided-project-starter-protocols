042294d3989b9a7ac540adda94c1cf262d07eb5c
Christopher DeVito <toolofjesus@protonmail.com>
2020-02-03 23:11:13 -0500

    Uploading completed guided project

--- Protocols.playground/Pages/Game.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Game.xcplaygroundpage/Contents.swift
@@ -32,21 +32,121 @@ class Dice {
 }
 
 //: Now, let's define a couple protocols for managing a dice-based game.
+protocol DiceGame {
+    var dice: Dice { get }
+    func play()
+}
 
+protocol DiceGameDelegate {
+    func gameDidStart(_ game: DiceGame)
+    func game(_ game: DiceGame, player: Player, didStartNewTurnWithDiceRoll diceRoll: Int)
+    func gameDidEnd(_ game: DiceGame)
+}
 
 
 //: Lastly, we'll create a custom class for tracking a player in our dice game.
+class Player {
+    let id: Int
+    let knockOutNumber: Int = Int.random(in: 6...9)
+    var score: Int = 0
+    var knockedOut: Bool = false
+    
+    init(id: Int) {
+        self.id = id
+    }
+}
 
 
 
 //: With all that configured, let's build our dice game class called _Knock Out!_
+class KnockOut: DiceGame {
+    var dice: Dice = Dice(sides: 6, generator: OneThroughTen())
+    var players: [Player] = []
+    var delegate: DiceGameDelegate?
+    
+    init(numberOfPlayers: Int) {
+        for i in 1...numberOfPlayers {
+            let aPlayer = Player(id: i)
+            players.append(aPlayer)
+        }
+    }
+    
+    func play() {
+        delegate?.gameDidStart(self)
+        var reachedGameEnd = false
+        
+        // We are going to play until the game is over
+        while !reachedGameEnd {
+            // Each player who has not been knowcked out gets a turn
+            for player in players where player.knockedOut == false {
+                //roll 2 6 sided dice and add together
+                let diceRollSum = dice.roll() + dice.roll()
+                
+                delegate?.game(self, player: player, didStartNewTurnWithDiceRoll: diceRollSum)
+                
+                // did i roll my knockout number?
+                if diceRollSum == player.knockOutNumber {
+                    // if i rolled my knockout number, this happens
+                    print("Player \(player.id) is knocked out by rolling: \(player.knockOutNumber)")
+                    // i've been knocked out
+                    player.knockedOut = true
+                    
+                    //if everyone is knocked out, game is over
+                    let activePlayers = players.filter( {$0.knockedOut == false })
+                    
+                    if activePlayers.count == 0 {
+                        // no more players - game over
+                        reachedGameEnd = true
+                        delegate?.gameDidEnd(self)
+                        print("All players have been knocked out!")
+                    }
+                } else {
+                    // I didn't get knockedo out, give me a higher score
+                    player.score += diceRollSum
+                    // Did I win?
+                    if player.score >= 100 {
+                        // if so, game is over
+                        reachedGameEnd = true
+                        delegate?.gameDidEnd(self)
+                        print("Player \(player.id) has won with a final score of \(player.score)")
+                    }
+                }
+            }
+        }
+    }
+}
 
+//let game = KnockOut(numberOfPlayers: 5)
+//game.play()
 
 
 //: The following class is used to track the status of the above game, and will conform to the `DiceGameDelegate` protocol.
-
+class DiceGameTracker: DiceGameDelegate {
+    var numberOfTurns = 0
+    
+    func gameDidStart(_ game: DiceGame) {
+        numberOfTurns = 0
+        if game is KnockOut {
+            print("Started a new game of Knock Out")
+        }
+        print("The game is using a \(game.dice.sides)-sided dice")
+    }
+    
+    func game(_ game: DiceGame, player: Player, didStartNewTurnWithDiceRoll diceRoll: Int) {
+        numberOfTurns += 1
+        print("Player #\(player.id) rolled a \(diceRoll)")
+    }
+    
+    func gameDidEnd(_ game: DiceGame) {
+        print("The game lasted for \(numberOfTurns) turns.")
+    }
+}
 
 
 //: Finally, we need to test out our game. Let's create a game instance, add a tracker, and instruct the game to play.
+let tracker = DiceGameTracker()
+let game = KnockOut(numberOfPlayers: 5)
 
+game.delegate = tracker
+game.play()
 
--- Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
@@ -6,17 +6,96 @@ import Foundation
 //: "... a blueprint of methods, properties, and other requirements that suit a particular task or piece of functionality. The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol."
 //:
 //: The below example shows a protocol that requires conforming types have a particular property defined.
+protocol FullyNamed {
+    var fullName: String { get }
+    
+}
+
+struct Person: FullyNamed {
+    var fullName: String
+}
+
+let johnny = Person(fullName: "Johnny Hicks")
+let spencer = Person(fullName: "Spencer Curtis")
 
 
+class Starship: FullyNamed {
+    var prefix: String?
+    var name: String
+    
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+    
+    // computed property - run a calculation when calling the getter
+    var fullName: String {
+        return (prefix != nil ? prefix! + " " : "") + name
+    }
+}
+
+var ncc1701 = Starship(name: "Enterprise", prefix: "USS")
+ncc1701.fullName
+
+var firefly = Starship(name: "Serenity")
+firefly.fullName
 
 //: Protocols can also require that conforming types implement certain methods.
+protocol GeneratesRandomNumbers {
+    func random() -> Int
+}
 
+//adopt
+class OneThroughTen: GeneratesRandomNumbers {
+    //conform
+    func random() -> Int {
+        return Int.random(in: 1...10)
+    }
+}
 
+let rand = OneThroughTen()
+rand.random()
+
+class OneThorughOneHundred: GeneratesRandomNumbers {
+    func random() -> Int {
+        return Int.random(in: 1...100)
+    }
+}
+
+let rand2 = OneThorughOneHundred()
+rand2.random()
 
 //: Using built-in Protocols
+extension Starship: Equatable {
+    static func == (lhs: Starship, rhs: Starship) -> Bool {
+        return lhs.fullName == rhs.fullName
+    }
+}
 
+if ncc1701 == firefly {
+    print("The ships are the same")
+} else {
+    print("the ships are different")
+}
 
 
 //: ## Protocols as Types
+class Dice {
+    let sides: Int
+    let generator: GeneratesRandomNumbers
+    
+    init(sides: Int, generator: GeneratesRandomNumbers) {
+        self.sides = sides
+        self.generator = generator
+    }
+    
+    func roll() -> Int {
+        return Int(generator.random() % sides) + 1
+    }
+}
 
+var d6 = Dice(sides: 6, generator: OneThroughTen())
+for _ in 1...5 {
+    print("Random dice roll is \(d6.roll())")
+}
 
